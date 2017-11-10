12d3a1b419a6d4ec24eae4d908a79e4b38009242
Nathan Ogden <nathan@ogden.tech>
2017-11-10 16:05:41 -0500

    fix to __main__ to work both with setuptools and standalone

--- platform_cc/__main__.py
+++ platform_cc/__main__.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
 import pkg_resources
 from cleo import Application
-from .app.commands.project_commands import ProjectStart, ProjectStop, ProjectProvision, ProjectDeploy, ProjectInfo, ProjectPurge
-from .app.commands.router_commands import RouterStart, RouterStop, RouterAdd, RouterRemove
-from .app.commands.var_commands import VarSet, VarGet, VarDelete, VarList
-from .app.commands.app_commands import AppShell
-from .app.commands.mysql_commands import MysqlSql, MysqlDump
+from app.commands.project_commands import ProjectStart, ProjectStop, ProjectProvision, ProjectDeploy, ProjectInfo, ProjectPurge
+from app.commands.router_commands import RouterStart, RouterStop, RouterAdd, RouterRemove
+from app.commands.var_commands import VarSet, VarGet, VarDelete, VarList
+from app.commands.app_commands import AppShell
+from app.commands.mysql_commands import MysqlSql, MysqlDump
 
 try:
     version = pkg_resources.require("platform_cc")[0].version
