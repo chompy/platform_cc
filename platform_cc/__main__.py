96736d0e3406cb2c894ea6fde9fffb20810c41cd
Nathan Ogden <nathanogden@chompy.me>
2017-11-10 15:00:41 -0500

    fix imports in __main__ for python3 setuptools

--- platform_cc/__main__.py
+++ platform_cc/__main__.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
 import pkg_resources
 from cleo import Application
-from app.commands.project_commands import ProjectStart, ProjectStop, ProjectProvision, ProjectDeploy, ProjectInfo, ProjectPurge
-from app.commands.router_commands import RouterStart, RouterStop, RouterAdd, RouterRemove
-from app.commands.var_commands import VarSet, VarGet, VarDelete, VarList
-from app.commands.app_commands import AppShell
-from app.commands.mysql_commands import MysqlSql, MysqlDump
+from .app.commands.project_commands import ProjectStart, ProjectStop, ProjectProvision, ProjectDeploy, ProjectInfo, ProjectPurge
+from .app.commands.router_commands import RouterStart, RouterStop, RouterAdd, RouterRemove
+from .app.commands.var_commands import VarSet, VarGet, VarDelete, VarList
+from .app.commands.app_commands import AppShell
+from .app.commands.mysql_commands import MysqlSql, MysqlDump
 
 try:
     version = pkg_resources.require("platform_cc")[0].version
