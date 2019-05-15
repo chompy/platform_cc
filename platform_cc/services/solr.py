49b9229a0adcb1019ff007edbe16600637708959
Nathan Ogden <nathan@ogden.tech>
2019-05-15 12:22:40 -0400

    added dummy solr service

---
+++ platform_cc/services/solr.py
@@ -0,0 +1,59 @@
+from .base import BasePlatformService
+import hashlib
+import base36
+import docker
+import time
+import requests
+
+
+class SolrService(BasePlatformService):
+    """
+    Handler for Solr service.
+    Currently a dummy service, does nothing.
+    """
+
+    """ Mapping for service type to Docker image name. """
+    DOCKER_IMAGE_MAP = {
+        "solr:3.6":            "busybox",
+        "solr:4.10":           "busybox",
+        "solr:6.3":            "busybox",
+        "solr:6.6":            "busybox",
+        "solr:7.6":            "busybox"
+    }
+
+    def getBaseImage(self):
+        return self.DOCKER_IMAGE_MAP.get(self.getType())
+
+    def getContainerVolumes(self):
+        return {
+            self.getVolumeName(): {
+                "bind": "/solr_data",
+                "mode": "rw"
+            }
+        }
+
+    def getServiceData(self):
+        data = BasePlatformService.getServiceData(self)
+        endpoints = self.config.get("endpoints", {})
+        # for solr version 4 automatically add "collection1" endpoint
+        # see https://docs.platform.sh/configuration/services/solr.html#solr-4
+        versionNo = self.getType().split(":")[1][0]
+        if versionNo == "4":
+            endpoints = {
+                "solr": {
+                    "core": "collection1"
+                }
+            }
+        for name, config in endpoints.items():
+            data["platform_relationships"][name.strip()] = {
+                "host":           self.getContainerName(),
+                "ip":             data.get("ip", ""),
+                "port":           8983,
+                "path":           "solr/%s" % config.get("core", "default"),
+                "scheme":         "solr"
+            }
+        return data
+
+    def start(self):
+        BasePlatformService.start(self)
+        # TODO
