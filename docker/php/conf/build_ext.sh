7f7dd4ea87c1078fdc6178d73e58dcc2d7a3299b
Nathan Ogden <nathan@ogden.tech>
2019-09-18 16:53:10 -0400

    fixed php 5 igbinary

--- docker/php/conf/build_ext.sh
+++ docker/php/conf/build_ext.sh
@@ -24,12 +24,13 @@ build_ext()
 if [ "$PHP_VER" = "5" ]; then
     build_ext "https://pecl.php.net/get/memcached-2.2.0.tgz" "memcached"
     build_ext "https://pecl.php.net/get/xdebug-2.5.5.tgz" "xdebug"
+    build_ext "https://pecl.php.net/get/igbinary-2.0.8.tgz" "igbinary"
 fi
 if [ "$PHP_VER" = "7" ]; then
     build_ext "https://pecl.php.net/get/memcached-3.1.3.tgz" "memcached"
     build_ext "https://pecl.php.net/get/xdebug-2.7.2.tg" "xdebug"
+    build_ext "https://pecl.php.net/get/igbinary-3.0.1.tgz" "igbinary"
 fi
 build_ext "https://pecl.php.net/get/redis-4.3.0.tgz" "redis"
-build_ext "https://pecl.php.net/get/igbinary-3.0.1.tgz" "igbinary"
 build_ext "https://pecl.php.net/get/imagick-3.4.4.tgz" "imagick"
 
