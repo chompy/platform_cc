59eca0d65d07f1ff34dc2dacb875bf394bfd95a0
Nathan Ogden <nathan@ogden.tech>
2021-02-18 16:02:20 -0500

    install local script to install all files from local repo

---
+++ scripts/install_local.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+INSTALL_PATH=~/.local/bin
+MAC_INSTALL_PATH=/usr/local/bin
+
+# change install path for mac
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    INSTALL_PATH="$MAC_INSTALL_PATH"
+fi
+
+# copy scripts
+cp $SCRIPTPATH/platform_sh_clone.sh $INSTALL_PATH/pcc_psh_sync
+cp $SCRIPTPATH/send_log.sh $INSTALL_PATH/pcc_send_log
+
+# build
+bash $SCRIPTPATH/build_local.sh
