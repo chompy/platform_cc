6c89c368655f9a5cdba9832f0b919b99ec9de7e5
Nathan Ogden <nathan@ogden.tech>
2021-02-25 11:36:28 -0500

    var:list command shows source of variables, global or project

--- cmd/variable.go
+++ cmd/variable.go
@@ -19,6 +19,7 @@ package cmd
 
 import (
 	"encoding/json"
+	"sort"
 
 	"gitlab.com/contextualcode/platform_cc/api/def"
 	"gitlab.com/contextualcode/platform_cc/api/output"
@@ -94,25 +95,39 @@ var varListCmd = &cobra.Command{
 		// global config
 		gc, err := def.ParseGlobalYamlFile()
 		handleError(err)
-		// build var list
-		varList := make(def.Variables)
-		varList.Merge(gc.Variables)
-		varList.Merge(proj.Variables)
 		// json out
 		jsonFlag := cmd.Flags().Lookup("json")
 		if jsonFlag != nil && jsonFlag.Value.String() != "false" {
+			// build var list
+			varList := make(def.Variables)
+			varList.Merge(gc.Variables)
+			varList.Merge(proj.Variables)
 			out, err := json.MarshalIndent(varList, "", "  ")
 			handleError(err)
 			println(string(out))
 			return
 		}
+
+		varSources := make(map[string][]string)
+		varKeys := make([]string, 0)
+		for _, k := range gc.Variables.Keys() {
+			varSources[k] = []string{"global", gc.Variables.GetString(k)}
+		}
+		for _, k := range proj.Variables.Keys() {
+			varSources[k] = []string{"project", proj.Variables.GetString(k)}
+		}
+		for k := range varSources {
+			varKeys = append(varKeys, k)
+		}
+		sort.Strings(varKeys)
+
 		// table out
 		data := make([][]string, 0)
-		for _, k := range varList.Keys() {
-			data = append(data, []string{k, varList.GetString(k)})
+		for _, k := range varKeys {
+			data = append(data, []string{k, varSources[k][0], varSources[k][1]})
 		}
 		drawTable(
-			[]string{"Key", "Value"},
+			[]string{"Key", "Source", "Value"},
 			data,
 		)
 	},
