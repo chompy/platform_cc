dfa0ad58c0f0e45c02b7854200d4593ad2d7c9c1
Nathan Ogden <nathan@ogden.tech>
2021-07-12 13:24:57 -0400

    added support for docker over ssh

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/containerd/containerd v1.5.2 // indirect
+	github.com/docker/cli v20.10.7+incompatible // indirect
 	github.com/docker/docker v20.10.7+incompatible
 	github.com/docker/go-connections v0.4.0
 	github.com/helloyi/go-sshclient v1.0.0
--- pkg/container/docker.go
+++ pkg/container/docker.go
@@ -18,8 +18,11 @@ along with Platform.CC.  If not, see <https://www.gnu.org/licenses/>.
 package container
 
 import (
+	"net/http"
+	"os"
 	"strings"
 
+	"github.com/docker/cli/cli/connhelper"
 	"github.com/docker/docker/client"
 	"github.com/pkg/errors"
 )
@@ -31,13 +34,51 @@ type Docker struct {
 
 // NewDocker creates a new Docker container handler.
 func NewDocker() (Docker, error) {
-	dockerClient, err := client.NewClientWithOpts(client.FromEnv)
+	dockerOpts, err := getDockerOpts()
+	if err != nil {
+		return Docker{}, errors.WithStack(err)
+	}
+	dockerClient, err := client.NewClientWithOpts(dockerOpts...)
 	if err != nil {
 		return Docker{}, errors.WithStack(convertDockerError(err))
 	}
 	return Docker{
 		client: dockerClient,
 	}, nil
+}
+
+func getDockerOpts() ([]client.Opt, error) {
+	// standard docker environment
+	host := os.Getenv("DOCKER_HOST")
+	if !strings.HasPrefix(host, "ssh://") {
+		return []client.Opt{client.FromEnv}, nil
+	}
+	// use ssh
+	// https://stackoverflow.com/a/57933792
+	helper, err := connhelper.GetConnectionHelper(host)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	httpClient := &http.Client{
+		// No tls
+		// No proxy
+		Transport: &http.Transport{
+			DialContext: helper.Dialer,
+		},
+	}
+	var clientOpts []client.Opt
+	clientOpts = append(clientOpts,
+		client.WithHTTPClient(httpClient),
+		client.WithHost(helper.Host),
+		client.WithDialContext(helper.Dialer),
+	)
+	version := os.Getenv("DOCKER_API_VERSION")
+	if version != "" {
+		clientOpts = append(clientOpts, client.WithVersion(version))
+	} else {
+		clientOpts = append(clientOpts, client.WithAPIVersionNegotiation())
+	}
+	return clientOpts, nil
 }
 
 // convertDockerError converts internal docker error to platform.cc error.
